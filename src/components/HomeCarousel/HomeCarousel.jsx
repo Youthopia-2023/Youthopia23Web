3ab708973e4112e2a9b81728042190eb9779a5fb
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-09-25 14:49:22 +0530

    added carousel

---
+++ src/components/HomeCarousel/HomeCarousel.jsx
@@ -0,0 +1,14 @@
+import "./HomeCarousel.css"
+
+const HomeCarousel = () => {
+  return (
+    <div className="homeCarousel">
+        <div className="topBar">
+            <div className="topBarLeft">Top Events</div>
+            <div className="topBarRight">Register Now</div>
+        </div>
+    </div>
+  )
+}
+
+export default HomeCarousel
