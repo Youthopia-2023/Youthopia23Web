ea58e49018f8abee6fb0b8922bb829b9c1f00d98
Harsheet Sharma <asarma626@gmail.com>
2023-09-25 23:17:33 +0530

    feat: Created the events page layout using the anchors and the css

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,9 +1,17 @@
 import "./Events.css"
+import { BrowserRouter,Route } from "react-router-dom"
+import Youthopia from '../../assets/youthopia.png'
 
 function Events() {
   return (
-    <div>
-      This is events page
+    <div className="events">
+      <img className="youImg" src={Youthopia} alt="Image not foud" />
+      <div className="secNavBar">
+        <a href="/events">Technical</a>
+        <a href="/events/cultural">Cultural</a>
+        <a href="/events/informal">Informal</a>
+      </div>
+      
     </div>
   )
 }
