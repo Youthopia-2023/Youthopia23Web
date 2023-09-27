16ea55f16175c65ebf3faea005d75bd6cc62fc80
Tanishq <tanishqjain1652003@gmail.com>
2023-09-27 16:00:21 +0530

    navbar first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <div id="navbar">
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/events"> Events</Link>
+                <Link to="/gallery"> Gallery </Link>
+                <Link to="/about"> About us</Link>
+            </div>
+            <div className="buttonsNav">
+                <button className="clickme1"> Log in </button>
+                <button className="clickme2"> Signup</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
