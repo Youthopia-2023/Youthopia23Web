c068c99e81ce0f497e8728a0b1e92cf8499598c2
Aachman <achmanagarwal7696@gmail.com>
2023-09-29 02:33:26 +0530

    elements added in login and signUp form

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React , { useState }  from 'react'
 import Youthopia from '../../assets/youthopia.png'
 import './SignUp.css';
+import star1 from "../../assets/Star 4.svg";
+import star2 from "../../assets/Group 6647.svg";
+import star3 from "../../assets/Star 4.svg";
+import line from "../../assets/Line 4.svg";
+import ellipse from "../../assets/Ellipse.svg";
+
+
 
 
 const SignUp = () => {
+  const [checked, setChecked] = useState(false);
   return (
+  
     <div className='signUp-container'>
         <div className="signUp">
+           <img src={star1} alt="star-1" className="star-1"></img>
+           <img src={star2} alt="star-2" className="star-2"></img>
+           <img src={star3} alt="star-3" className="star-3"></img>
+           <img src={line} alt="star-3" className="line-4"></img>
+           <img src={ellipse} alt="star-3" className="ellipse"></img>
+
             <img src={Youthopia}></img>
             <p>SIGN UP</p>
                 <form action="#">
@@ -18,7 +33,8 @@ const SignUp = () => {
                     <input type="password" placeholder="Password" />
                     <input type="password" placeholder="Confirm Password"/>
                     <label >
-                    <input type="checkbox" />
+                    <input type="checkbox" defaultChecked={checked}
+                           onChange={() => setChecked(!checked)}/>
                     I agree to this terms and condition
                     </label>
                     <input type="submit" />
