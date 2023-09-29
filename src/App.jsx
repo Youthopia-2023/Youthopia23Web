babd8bde7b7f0d00991600c4fa2a278f7f62e78e
Aachman <achmanagarwal7696@gmail.com>
2023-09-29 21:52:45 +0530

    Verify and confirm email page

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
 import Events from './pages/Events/Events';
 import Gallery from './pages/Gallery/Gallery';
-
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import EventInfo from './pages/EventInfo/EventInfo';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer'
 import IndividualEvent from './components/IndividualEvents/IndividualEvent';
+import VerifyEmail from './pages/VerifyEmail/VerifyEmail';
+import ConfirmEmail from './pages/ConfirmEmail/ConfirmEmail';
 
 
 function App() {
@@ -28,6 +29,8 @@ function App() {
             <Route path="/eventinfo" element={<EventInfo />} />
             <Route path="/signUp" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/verify" element={<VerifyEmail />} />
+            <Route path="/confirm" element={<ConfirmEmail />} />
             <Route path="*" element={<PageNotFound/>} />
 
             {/* <Route path='/login' element={<Login/>}/> */}
