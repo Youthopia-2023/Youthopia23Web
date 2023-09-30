ccf43d2b56385884d76bf08b934edc02b544de28
Tanishq <tanishqjain1652003@gmail.com>
2023-09-30 14:43:46 +0530

    video added on home screen

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -1,5 +1,6 @@
 import './VideoSection.css';
-import playicon from "../../assets/PlayIcon.svg";
+// import playicon from "../../assets/PlayIcon.svg";
+import testVideo from "../../assets/test.mp4"
 
 function VideoSection() {
   return (
@@ -7,7 +8,11 @@ function VideoSection() {
       <p className="map-heading">EXPERIENCE MORE</p>
       <div className="container">
         <div className="video-container">
-          <img src={playicon} alt="playicon"></img>
+          {/* <img src={playicon} alt="playicon"></img> */}
+          <video width="100%" height="100" controls>
+            <source src={testVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </div>
