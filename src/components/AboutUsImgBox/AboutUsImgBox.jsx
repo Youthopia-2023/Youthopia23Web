040461ef0fade51d6ff2c7e4bd9e9db57a8e4ac3
RudrakshGupta0413 <rudrakshgupta40@gmail.com>
2023-10-04 18:45:22 +0530

    About Page

--- src/components/AboutUsImgBox/AboutUsImgBox.jsx
+++ src/components/AboutUsImgBox/AboutUsImgBox.jsx
@@ -14,13 +14,13 @@ export default AboutUsImgBox;
 // import React from 'react';
 // import './AboutUsImgBox.css'; // Corrected the import statement
 
-// const AboutUsImgBox = ({ imageUrl, Names }) => { // Changed "Names" to "Names" for consistency
+// const AboutUsImgBox = ({ imageUrl, names }) => { // Changed "Names" to "Names" for consistency
 //   return (
 //     <div className="convenor">
 //       <div className="img-box">
 //         <img src={imageUrl} alt="Images" />
 //       </div>
-//       <p>{Names}</p>
+//       <p>{names}</p>
 //     </div>
 //   );
 // }
