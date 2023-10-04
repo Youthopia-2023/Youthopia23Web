44b50a881ab8e2f51281c4a61ea8f0c33b1d67f8
Aachman <achmanagarwal7696@gmail.com>
2023-10-04 19:22:58 +0530

    Registration comming soon

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './ConfirmEmail.css'
+import './Registration.css'
 import Youthopia from '../../assets/youthopia.png'
 import star1 from "../../assets/Star 4.svg";
 import star2 from "../../assets/Group 6647.svg";
@@ -7,10 +7,11 @@ import star3 from "../../assets/Star 4.svg";
 import line from "../../assets/Line 4.svg";
 import ellipse from "../../assets/Ellipse.svg";
 
-const ConfirmEmail = () => {
+
+const Registration = () => {
   return (
-    <div className='confirm-container'>
-        <div className="confirm">
+    <div className='registre-container'>
+        <div className="registre">
         <img src={star1} alt="star-1" className="star-1"></img>
         <img src={star1} alt="star-1" className="star-1"></img>
         <img src={star2} alt="star-2" className="star-2"></img>
@@ -19,8 +20,10 @@ const ConfirmEmail = () => {
         <img src={ellipse} alt="star-3" className="ellipse"></img>
 
         <img src={Youthopia} alt="" />
-        <p className='confirm-text'>To confirm your email address check your email and click the link to activate your account</p>
-         <button className="confirm-btn">Verify</button>
+
+        <p className='registre-text'>REGISTRATION WILL BE OPEN SOON!</p>
+         <button className="registre-btn">Back to Homepage</button>
+
 
         </div>
       
@@ -28,4 +31,4 @@ const ConfirmEmail = () => {
   )
 }
 
-export default ConfirmEmail
+export default Registration
