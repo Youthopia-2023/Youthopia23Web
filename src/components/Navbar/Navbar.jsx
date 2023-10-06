1c1b655375add17b16d46e1d8245e9b9fdc2b87e
Tanishq <tanishqjain1652003@gmail.com>
2023-10-07 01:31:06 +0530

    login signup route

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import "./Navbar.css"
 import { useEffect, useState } from "react";
 
 function Navbar() {
-    const [scrolling, setScrolling] = useState(false);
+  const navigate = useNavigate();
+  const navigateToLogin = () => {
+    navigate("/login");
+  }
+  const navigateToSignup = () => {
+    navigate("/signup");
+  }
+
+  const [scrolling, setScrolling] = useState(false);
 
   useEffect(() => {
     // Add an event listener to the window to track scrolling
@@ -23,22 +31,22 @@ function Navbar() {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-    
-    return (
-        <div className={`navbar ${scrolling ? 'blurred' : ''}`}>
-            <div className="links">
-                <NavLink exact to="/" activeClassName="active">Home</NavLink>
-                <NavLink to="/events" activeClassName="active"> Events</NavLink>
-                {/* <NavLink to="/gallery" activeClassName="active"> Gallery </NavLink> */}
-                <NavLink to="/about" activeClassName="active"> About us</NavLink>
-            </div>
-            <div className="buttonsNav">
-                <button  className="clickme1"> Log in </button>
-                <button className="clickme2"> Signup</button>
-            </div>
 
-        </div>
-    )
+  return (
+    <div className={`navbar ${scrolling ? 'blurred' : ''}`}>
+      <div className="links">
+        <NavLink exact to="/" activeClassName="active">Home</NavLink>
+        <NavLink to="/events" activeClassName="active"> Events</NavLink>
+        {/* <NavLink to="/gallery" activeClassName="active"> Gallery </NavLink> */}
+        <NavLink to="/about" activeClassName="active"> About us</NavLink>
+      </div>
+      <div className="buttonsNav">
+        <button className="clickme1" onClick={navigateToLogin}> Log in </button>
+        <button className="clickme2" onClick={navigateToSignup}> Signup</button>
+      </div>
+
+    </div>
+  )
 }
 
 export default Navbar
