02cc16fdcc80d86d5846c9c6b666ab554f136958
Tanishq <tanishqjain1652003@gmail.com>
2023-10-14 12:05:09 +0530

    changes in signup

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,5 @@
 import "./SignUp.css"
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Youthopia from "../../assets/youthopia.png";
 import "./SignUp.css";
 import star1 from "../../assets/Star 4.svg";
@@ -11,6 +11,12 @@ import ellipse from "../../assets/Ellipse.svg";
 import Footer from "../../components/Footer/Footer";
 import { AiOutlineCamera } from "react-icons/ai";
 function SignUp() {
+    const [file, setFile] = useState();
+    const handlephotoOnchange = (e) => {
+        setFileName(e.target.files[0].name);
+        setFile(e.target.files[0]);
+        console.log(file);
+    };
     const [checked, setChecked] = useState(false);
     const handleCheckbox = () => {
         setChecked(!checked);
@@ -21,15 +27,21 @@ function SignUp() {
         setCollege(e.target.value);
         console.log(college);
     }
+    const [fileName, setFileName] = useState("Identity proof (Aadhar, DL, etc)");
+    const ref = useRef(null);
+
+    const handleCamera = () => {
+        ref.current.click();
+    };
     return (
         <div>
             <div className="signUp-container">
-                <img className="youthopia-logo" src={Youthopia}/>
-                <img src={star1} alt="star-1" className="star-1Signup"/>
-                <img src={star2} alt="star-2" className="star-2Signup"/>
-                <img src={star3} alt="star-3" className="star-3Signup"/>
-                <img src={line} alt="star-3" className="line-4Signup"/>
-                <img src={ellipse} alt="star-3" className="ellipseSignup"/>
+                <img className="youthopia-logo" src={Youthopia} />
+                <img src={star1} alt="star-1" className="star-1Signup" />
+                <img src={star2} alt="star-2" className="star-2Signup" />
+                <img src={star3} alt="star-3" className="star-3Signup" />
+                <img src={line} alt="star-3" className="line-4Signup" />
+                <img src={ellipse} alt="star-3" className="ellipseSignup" />
                 <div className="signup-form">
                     <div className="nameRow">
                         <input type="text" placeholder="First Name" />
@@ -50,9 +62,9 @@ function SignUp() {
                                     <input type="text" value={college} placeholder="College Name" onChange={handleCollegeOnChange} />
                                 </div>
                                 <div className="identityPhoto">
-                                    {/* <input type="file" name="myfile" title = "ID card"/> */}
-                                    <div>Identity proof (Aadhar, DL, etc)</div>
-                                    <AiOutlineCamera />
+                                    <input type="file" onChange={handlephotoOnchange} ref={ref} className="choosefileHidden" name="" title="ID card" />
+                                    <div>{fileName}</div>
+                                    <AiOutlineCamera onClick={handleCamera}/>
                                 </div>
                                 <div className="idNumber">
                                     <input type="text" placeholder="Identity proof Number (Aadhar, DL, etc)" />
