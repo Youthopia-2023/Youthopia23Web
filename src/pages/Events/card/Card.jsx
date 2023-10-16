386e148e0b41f9a92d42b76129bf8cd75b9d0c92
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-10-17 00:33:00 +0530

    done registrations

--- src/pages/Events/card/Card.jsx
+++ src/pages/Events/card/Card.jsx
@@ -2,33 +2,36 @@
 import "./Card.css";
 import { Link } from "react-router-dom";
 
-
 export default function Card(props) {
   const style = {
-    margin: '30px 0 0 20%',
-    fontFamily: 'Stellar',
-    height: '50px',
-    color: 'black',
-    fontSize: '22px',
-    width: '80%',
-    borderRadius: '8px',
-    bottom: '50px',
-    backgroundColor: '#f8f8f8',
-    border: '2px solid BsSlack',
-  }
+    margin: "30px 0 0 20%",
+    fontFamily: "Stellar",
+    height: "50px",
+    color: "black",
+    fontSize: "22px",
+    width: "80%",
+    borderRadius: "8px",
+    bottom: "50px",
+    backgroundColor: "#f8f8f8",
+    border: "2px solid BsSlack",
+  };
   const datatopass = {
     id: props.id,
     name: props.name,
     venue: props.venue,
     date: props.date,
-    time: props.time,
+    start_time: props.start_time,
+    end_time: props.end_time,
+    min: props.min,
+    max: props.max,
     discription: props.discription,
     Coordinator: props.Coordinator,
     head: props.head,
-    fees: props.fees,
+    fees1: props.fees1,
+    fees2: props.fees2,
     imgUrl: props.imgUrl,
     category: props.category,
-  }
+  };
 
   // console.log(props)
   return (
@@ -43,8 +46,8 @@ export default function Card(props) {
           <p>{props.date}</p>
         </div>
         <div className="detail">
-          <p>{props.fees}</p>
-          <p>{props.time}</p>
+          <p>{props.fees1}</p>
+          <p>{props.start_time}</p>
         </div>
       </div>
 
