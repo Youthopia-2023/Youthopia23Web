6ec4c0cc487b79ebef1bf01cf2f8eda35a5348e1
Ashu-Shah1 <iamashu.shah12@gmail.com>
2023-10-18 15:27:03 +0530

    ticket

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -1,53 +1,38 @@
+import React from "react";
 import "./Ticket.css";
 import textYouth from "../../assets/youthopia.png";
 import BGticket from "../../assets/BGticket.svg";
 import youthopiaLogo from "../../assets/youthopiaLogo.svg";
 import ditlogowhite from "../../assets/ditlogowhite.svg";
-import { useLocation } from "react-router";
+
 
 function Ticket() {
-  let props = useLocation();
-  console.log(props);
   return (
     <div className="Ticket-container">
-      <img src={BGticket} className="rec" alt="" />
-
+      
+        <img src={BGticket} className="rec" alt="" />
+    
       <div className="Logos">
         <img src={youthopiaLogo} />
         <img src={ditlogowhite} />
       </div>
-      <div className="elements">
-        <div className="image1">
-          <img src={textYouth} className="Textyouth" />
-        </div>
-        <div className="Ticket-details">
-          <p className="head">Ticket Details</p>
-          <p>Event Name- {props.state.us.name}</p>
-          <p>Event Venue- {props.state.us.venue}</p>
-          <p>Event Date- {props.state.us.date}</p>
-          <p>Event Time- {props.state.us.start_time}</p>
-          <p>Ticket No- {props.state.ev}</p>
-        </div>
-      </div>
 
-      {/* <div>
-        <p className="Ticket">Ticket Details</p>
+      <div className="elements">
+
+        <div className="image1">
+        <img src={textYouth} className="Textyouth"/>
+        </div>
+
+        <div className="Ticket-details">
+         <p className="head">Ticket Details</p>
+         <p>Ticket Name-</p>
+         <p>Event Venue-</p>
+         <p>Event Date-</p>
+         <p>Event Time-</p>
+         <p>Ticket No:</p>
+        </div>
+        
       </div>
-      <div className="Details">
-        <p>Ticket Name-</p>
-        <p>Event Venue-</p>
-        <p>Event Time-</p>
-        <p>Ticket No-</p>
-      </div>
-      <div >
-        <img src={textYouth} className="Textyouth" />
-      </div>
-      <div>
-        <img src={Line}  className="Line"/>
-      </div>
-      <div>
-        <img src={Group} className="group"/>
-      </div> */}
     </div>
   );
 }
