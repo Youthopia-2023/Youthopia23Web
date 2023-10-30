5bbfbe82e5395f968668a54e89e2b21be62b9a20
Tanishq <tanishqjain1652003@gmail.com>
2023-10-30 13:13:59 +0530

    timer fixed

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -6,6 +6,7 @@ function Timer() {
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
+    const [isTimeOver, setIsTimeOver] = useState(false);
     setInterval(() => {
         const eventDate = new Date("2023-11-02T00:00:00");
         const currentDate = new Date();
@@ -14,37 +15,38 @@ function Timer() {
         const hours = Math.floor(totalSeconds / 3600) % 24;
         const minutes = Math.floor(totalSeconds / 60) % 60;
         const seconds = Math.floor(totalSeconds) % 60;
-        if(days<10){
-            setDays("0"+days);
+        if (days < 10) {
+            setDays("0" + days);
         }
-        else{
+        else {
             setDays(days);
         }
-        if(hours<10){
-            setHours("0"+hours);
+        if (hours < 10) {
+            setHours("0" + hours);
         }
-        else{
+        else {
             setHours(hours);
         }
-        if(minutes<10){
-            setMinutes("0"+minutes);
+        if (minutes < 10) {
+            setMinutes("0" + minutes);
         }
-        else{
+        else {
             setMinutes(minutes);
         }
-        if(seconds<10){
-            setSeconds("0"+seconds);
+        if (seconds < 10) {
+            setSeconds("0" + seconds);
         }
-        else{
+        else {
             setSeconds(seconds);
+        }
+        if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+            setIsTimeOver(true);
         }
     }, 1000);
 
-
-
     return (
         <div className="timer">
-            <div className="timer-container left">
+            {!isTimeOver && <><div className="timer-container left">
                 <div className="days">
                     <h1>{days}</h1>
                     <h2>Days</h2>
@@ -54,21 +56,20 @@ function Timer() {
                     <h2>Minutes</h2>
                 </div>
             </div>
-            <div className="centerLines">
-                <div className="line1"></div>
-                <div className="line2"></div>
-            </div>
-            <div className="timer-container">
-                <div className="hours">
-                    <h1>{hours}</h1>
-                    <h2>Hours</h2>
+                <div className="centerLines">
+                    <div className="line1"></div>
+                    <div className="line2"></div>
                 </div>
-                <div className="seconds">
-                    <h1>{seconds}</h1>
-                    <h2>Seconds</h2>
-                </div>
-
-            </div>
+                <div className="timer-container">
+                    <div className="hours">
+                        <h1>{hours}</h1>
+                        <h2>Hours</h2>
+                    </div>
+                    <div className="seconds">
+                        <h1>{seconds}</h1>
+                        <h2>Seconds</h2>
+                    </div>
+                </div></>}
         </div>
     )
 }
